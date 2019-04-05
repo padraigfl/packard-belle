37d59e85111c28a081d485d3143f4544ea51748a
padraigfl <padraigflood1@gmail.com>
2019-04-05 18:56:18 +0100

    resolving styles for css hover menu, restyled storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,9 +6,15 @@ import { withNotes } from '@storybook/addon-notes';
 import Theme from '../src/components/Theme';
 
 addDecorator(story => (
-  <div style={{ background: '#666666' }}>
-    <Theme style={{ height: '100vh', display: 'inline-block' }}>
-        {story()}
+  <div style={{ background: '#666666', padding: '4px' }}>
+    <Theme
+      style={{
+        height: '100vh',
+        display: 'inline-block',
+        backgroundColor: '#008080',
+      }}
+    >
+      {story()}
     </Theme>
   </div>
 ));
@@ -20,11 +26,11 @@ addDecorator(
     theme: {
       mainBackground: '#bbc3c4',
       mainBorder: '2px solid rgba(0,0,0,0)',
-      mainTextFace: "monospace",
+      mainTextFace: 'monospace',
       barFill: 'linear-gradient(to right, #0000a2, #126fc2)',
       barTextColor: 'white',
     },
-  }),
+  })
 );
 
 addDecorator(withNotes);
