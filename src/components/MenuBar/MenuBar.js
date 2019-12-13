ebdd76f612b311fbe79c6c4068defe7f5b0bd23b
padraigfl <padraigflood1@gmail.com>
2019-12-13 21:48:33 +0000

    flailing attempts to figure out menu bug issue

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -7,20 +7,24 @@ import './_MenuBar.scss';
 
 const MenuEntry = withMenuWrapper(Button);
 
-const MenuBar = props => (
-  <menu className="window__menu MenuBar">
-    {props.options &&
-      props.options.map(section => (
-        <MenuEntry
-          className={cx('MenuBar__section', props.className)}
-          key={`menu-bar-section-${section.title}`}
-          options={section.options}
-        >
-          {section.title}
-        </MenuEntry>
-      ))}
-  </menu>
-);
+const MenuBar = props => {
+  const { options, ...otherProps } = props;
+  return (
+    <menu className="window__menu MenuBar">
+      {options &&
+        options.map(section => (
+          <MenuEntry
+            {...otherProps}
+            className={cx('MenuBar__section', props.className)}
+            key={`menu-bar-section-${section.title}`}
+            options={section.options}
+          >
+            {section.title}
+          </MenuEntry>
+        ))}
+    </menu>
+  );
+};
 
 MenuBar.propTypes = {
   options: PropTypes.arrayOf(PropTypes.shape()),
